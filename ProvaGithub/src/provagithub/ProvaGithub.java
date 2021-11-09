88150a80bc8b1ec3fe28d4287fa8daedd153f086
Utente <Utente@Vito>
2021-11-09 17:20:51 +0100

    Seconda prova

--- ProvaGithub/src/provagithub/ProvaGithub.java
+++ ProvaGithub/src/provagithub/ProvaGithub.java
@@ -17,6 +17,7 @@ public class ProvaGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, world");
+        System.out.println("Hello, universe");
     }
     
 }
